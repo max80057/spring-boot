035a23a780e38ec451b590f7bbc69fa9c9904ee4
max80057 <max80057@gmail.com>
2020-04-09 21:24:30 +0800

    Add files via upload

---
+++ teddemo/src/test/java/com/example/teddemo/TeddemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.teddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello world");
+    }
+
+}
